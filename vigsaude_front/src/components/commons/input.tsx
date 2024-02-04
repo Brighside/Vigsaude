e7e4c1ba59e1d413abaf5e9e44e624666c25b95f
Brighside <2000Fellipe@gmail.com>
2024-02-04 20:42:21 -0300

    Correção no componente Input

--- vigsaude_front/src/components/commons/input.tsx
+++ vigsaude_front/src/components/commons/input.tsx
@@ -1,6 +1,6 @@
 import "../../styles/components/commons/input.css"
 
-export const Input = (props) => {
+export const Input = (props: any) => {
     return (
         <input type={props.type} placeholder={props.placeholder}/>
     )
