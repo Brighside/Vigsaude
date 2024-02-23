0ebd4f4dcc47faa47e94cf543884a1a9e8fe7a60
Brighside <2000Fellipe@gmail.com>
2024-02-23 15:33:45 -0300

    Alteração do title da Page.jsx e adição de variaveis css de cores

--- vigsaude_front/src/app/page.tsx
+++ vigsaude_front/src/app/page.tsx
@@ -1,5 +1,10 @@
 import { PainelLogin } from "@/components/login/painelLogin"
 import "./globals.css"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Vigsaude",
+}
 
 export default function Home() {
   return (
